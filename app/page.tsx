1a24a2c19e3f5d392530cc62ea8080b30d6390ab
Francis Frias <francisr.frias@gmail.com>
2024-02-13 15:54:11 +0800

    header

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,26 @@ import CustomerImage5 from '@/public/assets/img/customers/customer-5.jpg';
 import CustomerImage6 from '@/public/assets/img/customers/customer-6.jpg';
 
 const App = () => {
+  const imageMap = [
+    CustomerImage1,
+    CustomerImage2,
+    CustomerImage3,
+    CustomerImage4,
+    CustomerImage5,
+    CustomerImage6,
+  ].map((image, index) => {
+    return (
+      <Image
+        key={index}
+        src={image}
+        alt={`Customer Photo ${index + 1}`}
+        height={0}
+        width={0}
+        className='w-[6.4rem] h-[6.4rem] rounded-[50%] mr-[-1.6rem] last:mr-[3rem] ring-1 ring-tintWhite'
+      />
+    );
+  });
+
   return (
     <>
       <section className='bg-tintWhite py-[9.6rem] px-0'>
@@ -17,33 +37,31 @@ const App = () => {
             <h1 className='text-[5.2rem] font-[700] leading-[1.05] tracking-[-0.5px] mb-[3.2rem] text-[#333]'>
               A healthy meal delivered to your door, every single day
             </h1>
-
             <p className='text-[2rem] leading-[1.6] mb-[4.8rem] '>
               The smart 365-days-per-year food subscription that will make you
               eat healthy again. Tailored to your personal tastes and
               nutritional needs. We have delivered 250,000+ meals last year!
             </p>
-
             <a
               href='#'
               className='inline-block bg-primary text-white no-underline text-[2rem] font-semibold py-[1.6rem] px-[3.2rem] rounded-[9px] transition-colors duration-300 mr-6 hover:bg-shadeOrange'
             >
               Start eating well
             </a>
-
             <a
               href='#'
               className='inline-block bg-white text-accentDarkGray no-underline text-[2rem] font-semibold py-[1.6rem] px-[3.2rem] rounded-[9px]  transition-colors duration-300 hover:bg-tintWhite  hover:shadow-insideShadow'
             >
               Learn more &darr;
             </a>
-            <div className=''>
-              <div>
-                <Image src={CustomerImage1} alt={''} />
-              </div>
+            <div className='flex mt-32 items-center gap-[1.6rem]'>
+              <div className='flex'>{imageMap}</div>
+              <p className='text-[1.8rem] font-semibold '>
+                <span className='text-shadeOrange font-bold'>250,000+</span>{' '}
+                meals delivered last year!
+              </p>
             </div>
           </div>
-
           <div>
             <Image
               src={heroImage}
